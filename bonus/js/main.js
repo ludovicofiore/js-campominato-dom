bb6fc387c825495129f2952c62acdca2cf2a0363
ludoff <ludovicofiore25@gmail.com>
2024-06-14 17:47:59 +0200

    feat: bonus 1, adapt the exercise to the 3 level of difficulty

--- bonus/js/main.js
+++ bonus/js/main.js
@@ -2,6 +2,9 @@
 // creo variabile per select
 let gameMode = document.getElementById("select-mode");
 
+// dichiaro variabile per punteggio
+let score = 0;
+
 
 
 // creo click event per generare griglia
@@ -32,6 +35,11 @@ myButton.addEventListener("click",
 
         if (gameModeValue === "easy") { // se difficoltà facile
 
+
+            // creo array con numeri delle bombe
+            const bombsArray = createRandomNumArray(1, 100, 16);
+            console.log(bombsArray);
+
             // creo ciclo per le 100 caselle
             for (let i = 1; i <= 100; i++) {
 
@@ -55,14 +63,58 @@ myButton.addEventListener("click",
                         // aggiungo classe per cambiare colore 
                         littleSquare.classList.add("clicked");
 
-                        // mostro il numero della casella
+                        // mostro il numero della casella in console
                         console.log("numero casella: ", i);
+
+                        // richiamo contenitore risultato
+                        let result = document.querySelector(".result-container");
+
+                        // incremento punteggio per ogni click
+                        score++;
+                        console.log(score);
+
+                        // creo ciclo per estrapolare valori array bombe
+                        for (let index = 0; index < bombsArray.length; index++) {
+            
+                            let bombNumbers = bombsArray[index];
+                        
+
+                            // assegno le caselle bomba e condizione giocatore perdente
+                            if (i === bombNumbers) {
+                                littleSquare.classList.add("bomb");
+
+                                // creo elemeto per comunicare risultato
+                                let resultLoss = createElementWClass("h2", "title");
+                                result.append(resultLoss);
+
+                                // aggiungo contenuto e mostro punteggio
+                                resultLoss.innerHTML = "Hai perso, il tuo punteggio è: " + (score - 1);
+
+                            }
+                        
+                        }
+
+                        // condizione se giocatore vince 
+                        if (score === 84) {
+
+                            // creo elemento per comunicare risultato
+                            let resultWin = createElementWClass("h2", "title");
+                            result.append(resultWin);
+
+                            // aggiungo contenuto e mostro messaggio
+                            resultWin.innerHTML = "Hai vinto, il tuo punteggio è: " + score;
+                        }
 
                     }
                 );
             }
 
         } else if (gameModeValue === "mid") { // se difficoltà media
+
+
+            // creo array con numeri delle bombe
+            const bombsArray = createRandomNumArray(1, 81, 16);
+            console.log(bombsArray);
 
             // creo ciclo per le 81 caselle
             for (let i = 1; i <= 81; i++) {
@@ -90,11 +142,57 @@ myButton.addEventListener("click",
                         // mostro il numero della casella
                         console.log("numero casella: ", i);
 
+
+                        // richiamo contenitore risultato
+                        let result = document.querySelector(".result-container");
+
+                        // incremento punteggio per ogni click
+                        score++;
+                        console.log(score);
+
+                        // creo ciclo per estrapolare valori array bombe
+                        for (let index = 0; index < bombsArray.length; index++) {
+            
+                            let bombNumbers = bombsArray[index];
+                        
+
+                            // assegno le caselle bomba e condizione giocatore perdente
+                            if (i === bombNumbers) {
+                                littleSquare.classList.add("bomb");
+
+                                // creo elemeto per comunicare risultato
+                                let resultLoss = createElementWClass("h2", "title");
+                                result.append(resultLoss);
+
+                                // aggiungo contenuto e mostro punteggio
+                                resultLoss.innerHTML = "Hai perso, il tuo punteggio è: " + (score - 1);
+
+                            }
+                        
+                        }
+
+                        // condizione se giocatore vince 
+                        if (score === 65) {
+
+                            // creo elemento per comunicare risultato
+                            let resultWin = createElementWClass("h2", "title");
+                            result.append(resultWin);
+
+                            // aggiungo contenuto e mostro messaggio
+                            resultWin.innerHTML = "Hai vinto, il tuo punteggio è: " + score;
+                        }
+
                     }
                 );
             }
 
         } else if (gameModeValue === "hard") { //se difficoltà difficile
+
+
+            // creo array con numeri delle bombe
+            const bombsArray = createRandomNumArray(1, 49, 16);
+            console.log(bombsArray);
+
 
             // creo ciclo per le 49 caselle
             for (let i = 1; i <= 49; i++) {
@@ -121,6 +219,46 @@ myButton.addEventListener("click",
 
                         // mostro il numero della casella
                         console.log("numero casella: ", i);
+
+
+                        // richiamo contenitore risultato
+                        let result = document.querySelector(".result-container");
+
+                        // incremento punteggio per ogni click
+                        score++;
+                        console.log(score);
+
+                        // creo ciclo per estrapolare valori array bombe
+                        for (let index = 0; index < bombsArray.length; index++) {
+            
+                            let bombNumbers = bombsArray[index];
+                        
+
+                            // assegno le caselle bomba e condizione giocatore perdente
+                            if (i === bombNumbers) {
+                                littleSquare.classList.add("bomb");
+
+                                // creo elemeto per comunicare risultato
+                                let resultLoss = createElementWClass("h2", "title");
+                                result.append(resultLoss);
+
+                                // aggiungo contenuto e mostro punteggio
+                                resultLoss.innerHTML = "Hai perso, il tuo punteggio è: " + (score - 1);
+
+                            }
+                        
+                        }
+
+                        // condizione se giocatore vince 
+                        if (score === 33) {
+
+                            // creo elemento per comunicare risultato
+                            let resultWin = createElementWClass("h2", "title");
+                            result.append(resultWin);
+
+                            // aggiungo contenuto e mostro messaggio
+                            resultWin.innerHTML = "Hai vinto, il tuo punteggio è: " + score;
+                        }
 
                     }
                 );
@@ -150,5 +288,27 @@ function createElementWClass (tagName, className) {
     element.classList.add(className);
 
     return element
+}
+
+
+// funzione per array numeri casuali
+function createRandomNumArray (min, max, arrayLenght) {
+    
+    // creo array
+    const randomArray = [];
+
+    // creo ciclo per pushare i numeri
+    while (randomArray.length < arrayLenght) {
+
+        // creo numero random
+        let randomNum = Math.floor(Math.random() * (max - min +1)) + min;
+
+        // inserisco il numero nell'array se non già compreso
+        if (!randomArray.includes(randomNum)) {
+            randomArray.push(randomNum);
+        }
+    }
+
+    return randomArray
 }
 
